7e340decd6e245c4415ec5a1f5ad889c551add69
Rachana04-pr <lakshmimanju869@gmail.com>
2025-11-18 15:00:49 +0530

    Create function call user details.c

---
+++ function call user details.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int userdetails()
+{
+    printf("my name is:rachana\n");
+    printf("my age is:18\n");
+    printf("my branch is:CSE\n");
+}
+int main()
+{
+    userdetails();
+}
