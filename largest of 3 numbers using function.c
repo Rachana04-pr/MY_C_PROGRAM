e41253e20f4ed2abe7f398c2a6f3a2b5f3657e58
Rachana04-pr <lakshmimanju869@gmail.com>
2025-11-19 11:11:30 +0530

    Create largest of 3 numbers using function.c

---
+++ largest of 3 numbers using function.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int largest(int a,int b,int c)
+{
+    if(a>=c&& a>=c)
+    {
+        printf("a is largest number");
+    }
+    else if(b>=a&& b>=c)
+    {
+        printf("b is largest number");
+    }
+    else
+    {
+        printf("c is largest number");
+    }
+}
+int main()
+{
+    largest(2,3,5);
+}
