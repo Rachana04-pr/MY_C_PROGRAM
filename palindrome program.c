1a30f7d49cb7d2d67b8516c56a37813de10d29be
Rachana04-pr <lakshmimanju869@gmail.com>
2025-11-20 10:53:26 +0530

    Create palindrome program.c

---
+++ palindrome program.c
@@ -0,0 +1,19 @@
+int main()
+{
+    int num=121,original,rev=0,rem;
+    original=num;
+    while(num!=0)
+    {
+        rem=num%10;
+        rev=rev*10+rem;
+        num=num/10;
+    }
+    if(original==rev)
+    {
+        printf("It is a palindrom\n");
+    }
+    else
+    {
+        printf("It is not a palindrom\n");
+    }
+}
