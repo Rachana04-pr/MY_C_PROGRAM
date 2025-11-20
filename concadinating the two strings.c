52172fa1fe1f5e1fcefbde448bee878d8779a725
Rachana04-pr <lakshmimanju869@gmail.com>
2025-11-20 10:48:07 +0530

    Create concadinating the two strings.c

---
+++ concadinating the two strings.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char gopal[10];
+    printf("Enter the string\n");
+    scanf("%s",&gopal);
+    printf("%s",gopal);
+    strcat(gopal,"legend");
+}
+
